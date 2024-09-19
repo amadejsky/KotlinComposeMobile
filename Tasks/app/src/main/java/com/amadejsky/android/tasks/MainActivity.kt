9ba6040afe1102079a35af57926872e76009b59c
amadejsky <adrian.madejski@vp.pl>
2024-09-19 16:18:46 +0200

    Task project initialization, rows, positions and styling in Kotlin and compose

---
+++ Tasks/app/src/main/java/com/amadejsky/android/tasks/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.amadejsky.android.tasks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.amadejsky.android.tasks.ui.theme.TasksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+           MyColumn()
+        }
+    }
+    @Composable
+    fun MyColumn(){
+        Column(
+            modifier = Modifier.background(Color.LightGray).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ){
+            Text(text = "Hello Android!")
+            Text(text = "Is it me you looking for")
+            Text(text = "Test!")
+        }
+    }
+
+    @Composable
+    fun MyElement(){
+        Text(text = "Hello Android!")
+        Text(text = "Is it me you looking for")
+        Text(text = "Test!")
+    }
+}
+
